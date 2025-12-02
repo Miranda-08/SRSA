2824a0a7cbeec6ea3e394e4793357b32337dc18e
Kiritsugu-del <gabrielfurtadogf06@gmail.com>
2025-12-02 19:34:10 +0000

    Acho que esta n mudei quase nd

--- shelves.py
+++ shelves.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # Shelf simulator – PARTE 1
 # Estilo baseado em publisher01.py
-
+from influxdb_client_3 import flight_client_options
+import certifi
+from influxdb_client_3 import InfluxDBClient3, Point
 import paho.mqtt.client as mqtt
 import json
 import time
@@ -13,6 +15,20 @@ BROKER = "10.6.1.9"
 PORT = 1883
 USERNAME = "2023269477" #inventei, n sei se é arbitrário ou não...?
 PASSWORD = "srsa" #eu acho?
+
+
+fh = open(certifi.where(), "r")
+cert = fh.read()
+fh.close()
+
+
+#INFLUXDB Configuration
+
+token = "tCpqdhmLKj25M0W1Xt9F0_ok-nlk4hHPCPlDG6bjORsUdf23yWrpJgO9AidA6PZZfxn5G1JQ7i6u-b97s89sqQ=="
+org = "SRSA"
+host = "https://us-east-1-1.aws.cloud2.influxdata.com/"
+database = "SRSA_PROJECT"
+write_client = InfluxDBClient3(host=host, token=token, database=database, org=org, flight_client_options=flight_client_options(tls_root_certs=cert))
 
 
 def on_connect(client, userdata, flags, rc, properties):
@@ -33,10 +49,14 @@ def shelf_loop(GROUPID, zone_id, asset_id, update_time):
             "unit": "units", #corrigir?
             "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z")
         }
-
+        #===================MQTT====================
         topic = f"warehouse/{GROUPID}/locations/{zone_id}/{asset_id}/status"
         client.publish(topic, json.dumps(payload))
         print("[SHELF STATUS]", payload)
+        #===================INFLUXDB====================
+        p = Point("Data").tag("Shelves", f"{asset_id}").field("item_id",payload["item_id"] ).field("stock", payload["stock"]).field("units",payload['units']).time(payload["timestamp"])
+        write_client.write(p)
+        print(f"Values inserted")
 
         time.sleep(1)
 
