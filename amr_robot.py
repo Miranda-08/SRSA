cba8dda02b679128e7c6e0f999fb16c80e4e5d07
Miranda <140850122+Miranda-08@users.noreply.github.com>
2025-12-03 14:17:56 +0000

    tempos corretos & debug feature
    tempos corretos usando o time.monotonic() EXCETO o estado "MOVING TO PICK" que por alguma razão n respeita 1s por turno, temos de descobrir o erro

--- amr_robot.py
+++ amr_robot.py
@@ -1,47 +1,30 @@
-#!/usr/bin/env python3
-# AMR robot simulator – PARTE 1
-# Estilo baseado em publisher01.py | MQTT paho-mqtt 2.1.0
-
-
-# CORRIGIR SHELF-SX
-'''
-Command Processing & Logic: The script must use the struct library to decode
-the 3-byte binary payload received on the command topic (see "Warehouse
-Gateway" section). It must extract two target IDs to manage its deterministic
-cycle:
-■ Pick Phase: Upon receiving the command, the robot switches to
-6
-MOVING_TO_PICK. When the move time (3s) expires, it updates its
-location_id to SHELF-S{Byte2} (e.g., if Byte 2 is 0x0A, the location
-becomes "SHELF-S10").
-■ Drop Phase: After the pick wait time (1s) and the move-to-drop wait time
-(2s), the robot updates its location_id to STATION-P{Byte3} (e.g., if Byte
-3 is 0x01, the location becomes "STATION-P1").
-'''
+import paho.mqtt.client as mqtt, json, time, random, sys, struct, certifi
 from influxdb_client_3 import flight_client_options
-import certifi
 from influxdb_client_3 import InfluxDBClient3, Point
-import paho.mqtt.client as mqtt
-import json
-import time
-import random
-import sys
 from datetime import datetime, timezone
 
+
+# ********************************************* INICIALIZAÇÕES *********************************************
+battery = 100
+state = "IDLE"
+commands = [] #comandos por executar
+Sx = None #current shelf
+Px = None #current packing station
+remaining_time = 0 #segundos que faltam no estado atual
 
 fh = open(certifi.where(), "r")
 cert = fh.read()
 fh.close()
 
+interval = 1.0
+next_tick = time.monotonic()
 
-# MQTT Broker Configuration
+# ********************************************* CONFIG CONEXÕES *********************************************
+# ^ MQTT Broker Configuration
 BROKER = "10.6.1.9"
 PORT = 1883
-USERNAME = "2023269477" #inventei, n sei se é arbitrário ou não...?
-PASSWORD = "srsa" #eu acho?
 
-
-#INFLUXDB configuration
+# ^ INFLUXDB configuration
 token = "tCpqdhmLKj25M0W1Xt9F0_ok-nlk4hHPCPlDG6bjORsUdf23yWrpJgO9AidA6PZZfxn5G1JQ7i6u-b97s89sqQ=="
 org = "SRSA"
 host = "https://us-east-1-1.aws.cloud2.influxdata.com/"
@@ -49,153 +32,212 @@ database = "SRSA_PROJECT"
 write_client = InfluxDBClient3(host=host, token=token, database=database, org=org, flight_client_options=flight_client_options(tls_root_certs=cert))
 
 
-#varbiables and constants
-STATES = [
-        ("MOVING_TO_PICK", 3),
-        ("PICKING", 1),
-        ("MOVING_TO_DROP", 2),
-        ("DROPPING", 1)
-    ]
-global battery, state
-battery = 100
-state = "IDLE"
-
-
-
-
-
-received_task_via_MQTT = False#Mais sentido ser false e mudar on demmand 
-
+# ********************************************* FUNÇÕES CALLBACK *********************************************
 def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         print("[AMR] Connected to MQTT broker")
         topic_cmd = f"warehouse/{GROUPID}/amr/{robot_id}/command"
-        client.subscribe(topic_cmd, 1)  # obrigatório PAG. 5
-        print(f"[AMR] Subscribed to {topic_cmd} (Part 1 - ignored)")
+        client.subscribe(topic_cmd, 1)  # QoS 1: garante entrega pelo menos uma vez
+        print(f"[AMR] Subscribed to {topic_cmd}")
     else:
         print("[AMR] Connection failed:", rc)
 
-# Parte 1: comandos são ignorados (Parte 2 terá FORCE_CHARGE)
 def on_message(client, userdata, msg):
-    print(f"[AMR] (ignored) received command topic: {msg.topic}")
-    # Parte 1: não fazemos nada aqui.
-          
+    global commands
+    if msg.topic.endswith("/command"):
+        cmd_type, shelf_id, station_id = struct.unpack("BBB", msg.payload) # => unpack já os torna inteiros
+        commands.append((cmd_type, shelf_id, station_id))
+        print("[AMR] Received command:", cmd_type, shelf_id, station_id)
 
-def get_location(state):
+
+# ********************************************* FUNÇÕES AUXILIARES *********************************************
+def tick_sleep():
+    """
+    Garante que cada iteração do loop principal dura O MAIS PRÓXIMO POSSÍVEL de 1 segundo.
+
+    Para isso usamos um relógio monotónico, um contador que só avança e nunca é
+    ajustado pelo sistema (mudanças de hora, NTP, etc.), para medição de tempo + exata.
+
+    - CASO 1: A iteração foi RÁPIDA (<1s)
+    Cada vez que o loop acaba, esta função calcula quanto tempo FALTA
+    para completar 1 segundo desde a última iteração e dorme exatamente esse tempo
+    OU
+    - CASO 2: A iteração foi LENTA (>1s)
+    Cada vez que o loop acaba, a função calcula quanto tempo demorou em EXCESSO
+    em relação ao suposto 1s; não dorme neste caso e faz a próxima iteração começar
+    imediatamente para reduzir o atraso.
+
+    Isto impede que os atrasos se acumulem ao longo do tempo (sem “deriva”), e mantém
+    o loop a trabalhar num ritmo muito estável de 1 segundo por ciclo, mesmo que
+    a carga de trabalho varie entre iterações.
+    """
+    global next_tick, interval
+    next_tick += interval
+    sleep_time = next_tick - time.monotonic()
+    if sleep_time > 0:
+        time.sleep(sleep_time)
+
+def get_location(state, shelf_id, station_id):
     if state == "IDLE":                 return "DOCK"
     elif state.startswith("MOVING"):    return "TRANSIT"
-    elif state == "PICKING":            return "SHELF-Sx"          # EX: real shelves depend on order; part1 doesn't require real IDs
-    elif state == "DROPPING":           return "PACKING_ZONE"
+    elif state == "PICKING":            return f"SHELF-S{shelf_id}" if shelf_id is not None else "SHELF"
+    elif state == "DROPPING":           return f"STATION-P{station_id}" if station_id is not None else "STATION"
     elif state == "CHARGING":           return "CHARGING_STATION"
-    elif state == "STALLED":            return "TRANSIT"           # enunciado: fail only occurs while moving
+    elif state == "STALLED":            return "TRANSIT"
 
     else:                               return "UNKNOWN"
 
-
-
-def amr_state_machine(battery,state,STATES):
-        print(f"FIRST TURN")
-        print(f"\nNEW TURN")
-        # ================= STALLED ==============================
-        if state.startswith("MOVING") and random.random(0,1) < 0.05:
-            print("[AMR STATUS] Robot {robot_id} was moving but got STALLED for 10s... (coloquei stalled como status temporário APENAS para debug da parte 1; em teoria, stalled continua até que haja um high-priority override command)")
-            state = "STALLED"
-            time.sleep(10)
-            battery-=10
-            if battery < 0: battery = 0 #temporario
-
-        if state == "STALLED":
-            count -= 1
-            if battery == 100:
-                print(f"[AMR STATUS] Robot {robot_id} FULLY CHARGED. Returned to state IDLE.")
-                state = "IDLE"
-            return
-
-        # ================= CARREGAMENTO =========================
-        if battery <= 0 and state != "CHARGING":
-            print(f"[AMR STATUS] Robot {robot_id} is out of battery and will recharge for 10s...")
-            state = "CHARGING"
-
-        if state == "CHARGING":
-                if battery==100:
-                    print(f"[AMR STATUS] Robot {robot_id} FULLY CHARGED. Returned to state IDLE.")
-                    state = "IDLE"
-                return
-
-        # ================= TASK =========================
-        if state == "IDLE":
-            if received_task_via_MQTT: #por enquanto simulamos recebimento de uma task acho eu
-                print(f"[AMR STATUS] Robot {robot_id} received a FAKE (part 1) task.")
-                print(f"[AMR STATUS] Robot {robot_id} will start MOVING TO PICK for 3s...")
-                state = "MOVING_TO_PICK"
-                time.sleep(3)
-                battery -= 3
-                if battery < 0: battery = 0
-                return
-            else:
-                 time.sleep(1)
-                 battery-=1
-                 return
-
-        if state == "MOVING_TO_PICK":
-                print(f"[AMR STATUS] Robot {robot_id} arrived at picking spot.")
-                print(f"[AMR STATUS] Robot {robot_id} will start PICKING the item for 1s...")
-                state = "PICKING"
-                time.sleep(1)
-                battery -= 1
-                if battery < 0: battery = 0
-                return
-
-        if state == "PICKING":
-                print(f"[AMR STATUS] Robot {robot_id} picked the item.")
-                print(f"[AMR STATUS] Robot {robot_id} will start MOVING TO DROP for 1s...")
-                state = "MOVING_TO_DROP"
-                time.sleep(2)
-                battery -= 2
-                if battery < 0: battery = 0
-                return
-
-        if state == "MOVING_TO_DROP":
-                print(f"[AMR STATUS] Robot {robot_id} arrived at dropping spot.")
-                print(f"[AMR STATUS] Robot {robot_id} will start DROPPING for 1s...")
-                state = "DROPPING"
-                time.sleep(1)
-
-                battery -= 1
-                if battery < 0: battery = 0
-                return
-        
-
-        if state == "DROPPING":
-                print(f"[AMR STATUS] Robot {robot_id} dropped the item.")
-                print(f"[AMR STATUS] Robot {robot_id} enter IDLE state again")
-                state = "IDLE"
-                return
-
-
-def publish_info(robot_id,state,battery,GROUPID):
-            # ================= JSON =================================
+def publish_info(robot_id, GROUPID, state, battery, Sx, Px):
+        # ================= JSON =================================
         payload = {
             "robot_id": robot_id,
             "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
-            "location_id": get_location(state),
+            "location_id": get_location(state, Sx, Px),
             "battery": battery,
             "status": state
         }
-
         # ================= TOPIC PUBLISH =========================
         topic = f"warehouse/{GROUPID}/amr/{robot_id}/status"
         client.publish(topic, json.dumps(payload))
-        print("[AMR PAYLOAD] PAYLOAD PUBLISHED:", payload)
-
+        print("\n[AMR PAYLOAD] PAYLOAD PUBLISHED:", payload)
         #================= INFLUXDB ================================
-
-        p = Point("Data").tag("Robots", f"amr_robot{robot_id}").field("status",payload["status"] ).field("location", payload["location_id"]).field("battery",payload['battery']).time(payload["timestamp"])
+        p = (
+            Point("Data")
+            .tag("Robots", f"amr_robot{robot_id}")
+            .field("status",payload["status"] )
+            .field("location", payload["location_id"])
+            .field("battery",payload['battery'])
+            .time(payload["timestamp"])
+        )
         write_client.write(p)
-        print(f"Values inserted")
+        print(f"[INFLUX DB] Values inserted to InfluxDB: {p}")
 
 
-# ========= MAIN =========
+# ********************************************* CÓDIGO PRINCIPAL *********************************************
+DEBUG = True # <----------------------------------------------------------------------------------------------------------- DEBUG
+
+def amr_state_machine(robot_id, GROUPID):
+    global state, battery, commands, Sx, Px, remaining_time, next_tick, interval
+
+    time.sleep(1) # Tempo para deixar MQTT conectar antes do loop
+    # ========= DEBUG MODE ==========
+    if DEBUG:
+        print("*"*10 + "[DEBUG] Modo de debug ativo — inicializando comando fake" + "*"*10)
+        commands.append((1, 2, 5)) # cmd_type=1, shelf 2, station 5
+    # ===============================
+
+    print(f"\n\nSTART\n")
+    publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+    while True:
+        print(f"\n\nNEW TURN\n")
+
+        # ================= STALLED ==============================
+        if state.startswith("MOVING") and random.random() < 0.05:
+            print(f"[AMR STATUS] Robot {robot_id} was moving but got STALLED for 10s...")
+            #(stalled como status temporário APENAS para debug da parte 1; em teoria, stalled continua até que haja um high-priority override command)
+            state = "STALLED"
+            remaining_time = 10
+
+        if state == "STALLED":
+            #TEMPORÁRIO, PARA A PARTE 1: sai sozinho ao fim de 10s
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} saiu do estado STALLED por um milagre de Deus (TEMP - Part 1). Retorna para estado IDLE")
+                state = "IDLE"
+            else:
+                remaining_time -= 1
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+
+        # ================= CARREGAMENTO =========================
+        if battery <= 15 and state != "CHARGING":
+            print(f"[AMR STATUS] Robot {robot_id} is almost out of battery and will recharge for 10s...")
+            state = "CHARGING"
+            remaining_time = 10
+
+        if state == "CHARGING":
+            remaining_time -= 1
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} FULLY CHARGED. Returned to state IDLE.")
+                battery = 100
+                state = "IDLE"
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+
+        # ================= TASK =========================
+        if state == "IDLE":
+            if commands:
+                cmd_type, Sx, Px = commands.pop(0)
+                    #cmd_type 1 = EXECUTE_TASK, 3 = FORCE_CHARGE
+                print(f"[AMR STATUS] Robot {robot_id} received a [FAKE - part 1] task: TYPE {cmd_type}, SHELF {Sx}, STATION {Px}")
+                print(f"[AMR STATUS] Robot {robot_id} will start MOVING TO PICK for 3s... => shelf {Sx}")
+                state = "MOVING_TO_PICK"
+                remaining_time = 3
+            else:
+                publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+                tick_sleep()
+                continue
+
+        if state == "MOVING_TO_PICK":
+            battery = max(0, battery-1)
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} arrived at at picking shelf S{Sx}.")
+                print(f"[AMR STATUS] Robot {robot_id} will start PICKING item for 1s...")
+                state = "PICKING"
+                remaining_time = 1
+            else:
+                remaining_time = max(0, remaining_time-1)
+
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+
+        if state == "PICKING":
+            battery = max(0, battery-1)
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} picked the item.")
+                print(f"[AMR STATUS] Robot {robot_id} will start MOVING TO DROP for 2s... => packing station {Px}")
+                state = "MOVING_TO_DROP"
+                remaining_time = 2
+            else:
+                remaining_time = max(0, remaining_time-1)
+
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+
+        if state == "MOVING_TO_DROP":
+            battery = max(0, battery-1)
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} arrived at packing station P{Px}.")
+                print(f"[AMR STATUS] Robot {robot_id} will start DROPPING for 1s...")
+                state = "DROPPING"
+                remaining_time = 1
+            else:
+                remaining_time = max(0, remaining_time-1)
+
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+        
+        if state == "DROPPING":
+            battery = max(0, battery-1)
+            if remaining_time == 0:
+                print(f"[AMR STATUS] Robot {robot_id} dropped the item at station P{Px}.")
+                print(f"[AMR STATUS] Robot {robot_id} entering IDLE state again")
+                state = "IDLE"
+            else:
+                remaining_time = max(0, remaining_time-1)
+
+            publish_info(robot_id, GROUPID, state, battery, Sx, Px)
+            tick_sleep()
+            continue
+        
+        tick_sleep()
+        
+
+# ********************************************* MAIN *********************************************
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python3 amr_robot.py <GroupID> <RobotID>")
@@ -205,14 +247,10 @@ if __name__ == "__main__":
     robot_id = sys.argv[2]
 
     client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-    client.username_pw_set(USERNAME, PASSWORD)
     client.on_connect = on_connect
+    client.on_message = on_message
 
     client.connect(BROKER, PORT)
     client.loop_start()
 
-    while True:
-        amr_state_machine()
-        publish_info(robot_id,state,battery,GROUPID)
-        time.sleep(1)
-
+    amr_state_machine(robot_id, GROUPID)
